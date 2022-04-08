b8845cc6a08ea10a2ae31e86983393aa41dd6609
Georg Schramm <georg.schramm@posteo.net>
2022-04-08 12:42:36 +0200

    add multi resolution approach (different Gaussian filters before metrics / stat calc.) and support for vector valued metrics / stats

--- ImageQualityCallback.py
+++ ImageQualityCallback.py
@@ -1,14 +1,11 @@
 #TODO: - getter and setter methods
 
 # Discussion Tue afternoon:
-# - implement metrics / statistics that work on ImageData?
-# - handle non-scalar return values
-# - add pre-processing function that is called before calculating metrics / stats
+# - implement metrics / statistics that work on ImageData -> requires a bit more thinking for ROI results
 
 import numpy as np
 import cil.framework
 import tensorboardX
-
 from scipy.ndimage import gaussian_filter
 
 def MSE(x,y):
@@ -55,22 +52,34 @@ class ImageQualityCallback:
       the reference image.
       
     metrics_dict : dictionary of lambda functions f(x,y) mapping
-      two 1-dimensional numpy arrays x and y to a scalar value.
+      two 1-dimensional numpy arrays x and y to a scalar value or a
+      numpy.ndarray.
       x and y can be the voxel values of the whole images or the values of
       voxels in a ROI such that the metric can be computed on the whole
       images and optionally in the ROIs separately.
 
       E.g. f(x,y) could be MSE(x,y), PSNR(x,y), MAE(x,y)
 
+      If the return value is an nd.array, the results will be saved
+      as separate scalar values in the tensorboard results
+      (one for each value in the array)
+
     statistics_dict : dictionary of lambda functions f(x) mapping a 
-      1-dimensional numpy array x to a scalar value.
+      1-dimensional numpy array x to a scalar value or a numpy.ndarray.
       E.g. mean(x), std_deviation(x) that calculate global and / or
       ROI mean and standard deviations.
 
-    statistics_dict : list of floats
+      E.g. f(x) could be x.mean()
+
+      If the return value is an nd.array, the results will be saved
+      as separate scalar values in the tensorboard results
+      (one for each value in the array)
+
+    post_smoothing_fwhms_mm_list : list of floats
       Containing FWHMs (mm) of Gaussian post smoothing kernels to be applied
       before calculating all metrics and statistics.
-      Default [5., 8.]
+      If you don't want any of the post-smoothed metrics, pass an empty list.
+      Default []
 
 
     """
@@ -79,7 +88,7 @@ class ImageQualityCallback:
                        roi_mask_dict   = None,
                        metrics_dict    = None,
                        statistics_dict = None,
-                       post_smoothing_fwhms_mm_list = [5., 8.]):
+                       post_smoothing_fwhms_mm_list = []):
     
         # the reference image
         self.reference_image = reference_image
@@ -109,7 +118,8 @@ class ImageQualityCallback:
 
     def eval(self, iteration, last_cost , test_image):
         r""" Callback function called by CIL algorithm that calculates global and local
-             metrics and measures
+             metrics and measures.
+             The input arguments are fixed by the callback from the CIL algorithm class.
 
         Parameters
         ----------
@@ -125,6 +135,9 @@ class ImageQualityCallback:
 
         """
        
+        # (0) log the value of the cost function
+        self.tb_summary_writer.add_scalar('cost', last_cost, iteration)
+
         # get numpy arrays behind test ImageData
         test_image_array      = test_image.as_array()
         reference_image_array = self.reference_image.as_array()
@@ -146,17 +159,29 @@ class ImageQualityCallback:
 
             # (1) calculate global metrics and statistics
             global_metrics    = {}
-            global_statistics = {}
-
             if self.metrics_dict is not None:
                 for metric_name, metric in self.metrics_dict.items():
-                    global_metrics[metric_name] = metric(test_image_array_ps.ravel(), 
-                                                         reference_image_array_ps.ravel())
+                    met = metric(test_image_array_ps.ravel(), reference_image_array_ps.ravel())
+                    # check if metric is scalar or vector valued
+                    # for the 2nd case, we save each scalar value separately in the dict
+                    if isinstance(met, np.ndarray):
+                        for im, m in enumerate(met.ravel()):
+                            global_metrics[f'{metric_name}_{im}'] = m
+                    else:
+                        global_metrics[metric_name] = met
                 self.tb_summary_writer.add_scalars(f'global_metrics{ps_str}', global_metrics, iteration)
 
+            global_statistics = {}
             if self.statistics_dict is not None:
                 for statistic_name, statistic in self.statistics_dict.items():
-                    global_statistics[statistic_name] = statistic(test_image_array_ps.ravel())
+                    stat = statistic(test_image_array_ps.ravel())
+                    # check if statistic is scalar or vector valued
+                    # for the 2nd case, we save each scalar value separately in the dict
+                    if isinstance(stat, np.ndarray):
+                        for ist, st in enumerate(stat.ravel()):
+                            global_statistics[f'{statistic_name}_{ist}'] = st
+                    else:
+                        global_statistics[statistic_name] = stat
                 self.tb_summary_writer.add_scalars(f'global_statistics{ps_str}', global_statistics, iteration)
   
             # (2) caluclate local metrics and statistics
@@ -167,17 +192,28 @@ class ImageQualityCallback:
 
                     if self.metrics_dict is not None:
                         for metric_name, metric in self.metrics_dict.items():
-                            roi_metrics[metric_name] = metric(test_image_array_ps[roi_inds], 
-                                                              reference_image_array_ps[roi_inds])
+                            roi_met = metric(test_image_array_ps[roi_inds], reference_image_array_ps[roi_inds])
+                            # check if metric is scalar or vector valued
+                            # for the 2nd case, we save each scalar value separately in the dict
+                            if isinstance(met, np.ndarray):
+                                for im, m in enumerate(roi_met.ravel()):
+                                    roi_metrics[f'{metric_name}_{im}'] = m
+                            else:
+                                roi_metrics[metric_name] = roi_met
                         self.tb_summary_writer.add_scalars(f'{roi_name}_metrics{ps_str}', roi_metrics, iteration)
 
                     if self.statistics_dict is not None:
                         for statistic_name, statistic in self.statistics_dict.items():
-                            roi_statistics[statistic_name] = statistic(test_image_array_ps[roi_inds])
+                            roi_stat = statistic(test_image_array_ps[roi_inds])
+                            # check if statistic is scalar or vector valued
+                            # for the 2nd case, we save each scalar value separately in the dict
+                            if isinstance(roi_stat, np.ndarray):
+                                for ist, st in enumerate(roi_stat.ravel()):
+                                    roi_statistics[f'{statistic_name}_{ist}'] = st
+                            else:
+                                roi_statistics[statistic_name] = roi_stat
                         self.tb_summary_writer.add_scalars(f'{roi_name}_statistics{ps_str}', roi_statistics, iteration)
 
-            # (3) log the value of the cost function
-            self.tb_summary_writer.add_scalar('cost', last_cost, iteration)
 #------------------------------------------------------------------------------------------------------
 
 
@@ -226,7 +262,9 @@ if __name__ == '__main__':
                                              metrics_dict = {'MSE':MSE, 'MAE':MAE, 'PSNR':PSNR},
                                              statistics_dict = {'MEAN': (lambda x: x.mean()),
                                                                 'STDDEV': (lambda x: x.std()),
-                                                                'MAX': (lambda x: x.max())})
+                                                                'MAX': (lambda x: x.max()),
+                                                                'COM': (lambda x: np.array([3,2,1]))},
+                                             post_smoothing_fwhms_mm_list = [5., 8.])
 
     img_qual_callback.eval(1, 1, test_image)
     img_qual_callback.eval(2, 1, test_image*1.1)
