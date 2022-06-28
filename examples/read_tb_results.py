269a1b305f7615a6159171b233f478a1a26e5541
Georg Schramm <georg.schramm@posteo.net>
2022-06-28 14:10:06 +0200

    adapt read tensorboard example to new single file output

--- examples/read_tb_results.py
+++ examples/read_tb_results.py
@@ -1,79 +1,27 @@
-# minimal example that shows how to read tensorboard results into pandas data frames
-#
-# structure of the tensorboard experiment result folder created by the ImageQualityCallback 
-# containing the local and global image metrics and statistics should look like below
-#
-# the first event file contains the cost. 
-# all other contain the global/local image metrics and statistics
-#
-#
-# |-- events.out.tfevents.1649262033.amun
-# |-- global_metrics
-# |   |-- MAE
-# |   |   `-- events.out.tfevents.1649262033.amun
-# |   |-- MSE
-# |   |   `-- events.out.tfevents.1649262033.amun
-# |   `-- PSNR
-# |       `-- events.out.tfevents.1649262033.amun
-# |-- global_statistics
-# |   |-- MEAN
-# |   |   `-- events.out.tfevents.1649262033.amun
-# |   `-- STDDEV
-# |       `-- events.out.tfevents.1649262033.amun
-# |-- roi_1_metrics
-# |   |-- MAE
-# |   |   `-- events.out.tfevents.1649262033.amun
-# |   |-- MSE
-# |   |   `-- events.out.tfevents.1649262033.amun
-# |   `-- PSNR
-# |       `-- events.out.tfevents.1649262033.amun
-# |-- roi_1_statistics
-# |   |-- MEAN
-# |   |   `-- events.out.tfevents.1649262033.amun
-# |   `-- STDDEV
-# |       `-- events.out.tfevents.1649262033.amun
-# |-- roi_2_metrics
-# |   |-- MAE
-# |   |   `-- events.out.tfevents.1649262033.amun
-# |   |-- MSE
-# |   |   `-- events.out.tfevents.1649262033.amun
-# |   `-- PSNR
-# |       `-- events.out.tfevents.1649262033.amun
-# `-- roi_2_statistics
-#     |-- MEAN
-#     |   `-- events.out.tfevents.1649262033.amun
-#     `-- STDDEV
-#         `-- events.out.tfevents.1649262033.amun
-#
-#
-#
-#
-#----------------------------------------------------------------------------------------------
+# minimal example that shows how to read tensorboard results into one big pandas data frames
 
 import pandas as pd
 from tensorboard.backend.event_processing import event_accumulator
 from pathlib import Path
 
-# set this to the path of your experiment
-exp_path = sorted(list(Path('runs').glob('exp-*')))[-1]
-
 #----------------------------------------------------------------------------------------------
+event_file = sorted(list(Path('runs').glob('exp-*/events.out.tfevents.*')))[-1]
 
-# find all event files, loop over them and read them
-for event_file in sorted(list(exp_path.rglob('events.out.*'))):
-    print(event_file)
-    if event_file.parent == exp_path:
-        print('cost')
+ea = event_accumulator.EventAccumulator(str(event_file))
+ea.Reload()
+
+df = pd.DataFrame()
+
+for i, scalar_tag in enumerate(ea.Tags()['scalars']):
+    if i == 0:
+        df = pd.DataFrame(ea.Scalars(scalar_tag)).rename({'value':scalar_tag}, axis = 1)
     else:
-        print(event_file.parent.parent.name)
-        print(event_file.parent.name)
+        # we have to drop the wall_time since it is slightly different for every scalar tag
+        tmp_df = pd.DataFrame(ea.Scalars(scalar_tag)).rename({'value':scalar_tag}, axis = 1).drop(columns = 'wall_time')
+        df = pd.merge(df, tmp_df, on = 'step')
 
-    ea = event_accumulator.EventAccumulator(str(event_file))
-    ea.Reload()
-    
-    for scalar_tag in ea.Tags()['scalars']:
-        #print(scalar_tag)
-        df = pd.DataFrame(ea.Scalars(scalar_tag))
-        print(df)
+print(df.columns)
+print(df)
 
-    print('')
+# to plot a metric / statistic vs step use:
+# df.plot(x = 'step', y = 'Global_MEAN')
