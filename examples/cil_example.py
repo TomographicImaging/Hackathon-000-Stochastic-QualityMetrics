fee1a79ffd2b2dcb6ffc51e73ac08f689f14cbdf
Georg Schramm <georg.schramm@posteo.net>
2022-06-28 12:29:52 +0200

    update cil example to new package structure

--- examples/cil_example.py
+++ examples/cil_example.py
@@ -3,7 +3,7 @@ import cil.framework
 import tensorboardX
 from datetime import datetime
 
-from ImageQualityCallback import ImageQualityCallback
+import img_quality_cil_stir as imgq
 
 def MSE(x,y):
     """ mean squared error between two numpy arrays
@@ -57,17 +57,16 @@ roi_image_dict['roi_2'] = cil.framework.ImageData(array = mask_2, geometry = ima
 dt_string = datetime.now().strftime("%Y%m%d-%H%M%S")
 tb_summary_writer = tensorboardX.SummaryWriter(f'runs/exp-{dt_string}')
 # instanciate ImageQualityCallback
-img_qual_callback = ImageQualityCallback(ref_image, tb_summary_writer,
-                                         roi_mask_dict = roi_image_dict,
-                                         metrics_dict = {'MSE':MSE, 'MAE':MAE, 'PSNR':PSNR},
-                                         statistics_dict = {'MEAN': (lambda x: x.mean()),
-                                                            'STDDEV': (lambda x: x.std()),
-                                                            'MAX': (lambda x: x.max()),
-                                                            'COM': (lambda x: np.array([3,2,1]))},
-                                         post_smoothing_fwhms_mm_list = [5., 8.])
+img_qual_callback = imgq.ImageQualityCallback(ref_image, tb_summary_writer,
+                                              roi_mask_dict = roi_image_dict,
+                                              metrics_dict = {'MSE':MSE, 'MAE':MAE, 'PSNR':PSNR},
+                                              statistics_dict = {'MEAN': (lambda x: x.mean()),
+                                                                 'STDDEV': (lambda x: x.std()),
+                                                                 'MAX': (lambda x: x.max()),
+                                                                 'COM': (lambda x: np.array([3,2,1]))},
+                                              post_smoothing_fwhms_mm_list = [5., 8.])
 img_qual_callback.eval(1, 1, test_image)
 img_qual_callback.eval(2, 1, test_image*1.1)
 img_qual_callback.eval(3, 1, test_image*0.9)
 img_qual_callback.eval(4, 1, test_image*1.2)
 tb_summary_writer.close()
-
