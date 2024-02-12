7a05d86eb143cdb7490a1007704dcc636cd0b77f
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-02-12 18:39:49 +0000

    added tomography example

--- examples/recon_example.py
+++ examples/recon_example.py
@@ -8,10 +8,34 @@ ground_truth = dataexample.SIMULATED_SPHERE_VOLUME.get()
 
 data = dataexample.SIMULATED_CONE_BEAM_DATA.get()
 absorption = TransmissionAbsorptionConverter()(data)
+absorption = absorption.get_slice(vertical='centre')
+
+ig = absorption.geometry.get_ImageGeometry()
 
 #%%
-recon = FDK(absorption, image_geometry=data.geometry.get_ImageGeometry()).run()
+recon = FDK(absorption, image_geometry=ig).run()
 #%%
 show2D([ground_truth, recon], title = ['Ground Truth', 'FDK Reconstruction'], origin = 'upper', num_cols = 2)
 
+# %%
+from cil.plugins.tigre import ProjectionOperator
+from cil.optimisation.algorithms import FISTA 
+from cil.optimisation.functions import LeastSquares, IndicatorBox, ZeroFunction
+from cil.optimisation.operators import GradientOperator
+from cil.optimisation.utilities import callbacks
+
+#%%
+A = ProjectionOperator(image_geometry=ig, 
+                       acquisition_geometry=absorption.geometry)
+
+F = LeastSquares(A = A, b = absorption)
+G = IndicatorBox(lower=0)
+
+algo = FISTA(initial = ig.allocate(), f = F, g = G)
+
+#%%
+algo.run(10, callbacks=[callbacks.ProgressCallback()])
+# %%
+
+show2D([ground_truth, algo.solution], title = ['Ground Truth', 'FISTA Reconstruction'], origin = 'upper', num_cols = 2)
 # %%
