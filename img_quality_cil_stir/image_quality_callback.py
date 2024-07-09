5ac7902e75223e9c9785314fffbfc8b251215b11
Casper da Costa-Luis <casper.dcl@physics.org>
2024-07-09 12:26:27 +0100

    DRYer

--- img_quality_cil_stir/image_quality_callback.py
+++ img_quality_cil_stir/image_quality_callback.py
@@ -43,20 +43,16 @@ class ImageQualityCallback(Callback):
 
     Parameters
     ----------
-
     reference_image : CIL or STIR ImageData
       containing the reference image used to calculate the metrics
-
     tb_summary_writer : tensorboardX SummaryWriter
       summary writer used to log results to tensorboard event files
-
     roi_mask : dictionary of ImageData objects
       list containing one binary ImageData object for every ROI to be
       evaluated. Voxels with values 1 are considered part of the ROI
       and voxels with value 0 are not.
       Dimension of the ROI mask images must be the same as the dimension of
       the reference image.
-
     metrics : dictionary of lambda functions f(x,y) mapping
       two 1-dimensional numpy arrays x and y to a scalar value or a
       numpy.ndarray.
@@ -69,7 +65,6 @@ class ImageQualityCallback(Callback):
       If the return value is an nd.array, the results will be saved
       as separate scalar values in the tensorboard results
       (one for each value in the array)
-
     statistics : dictionary of lambda functions f(x) mapping a
       1-dimensional numpy array x to a scalar value or a numpy.ndarray.
       E.g. mean(x), std_deviation(x) that calculate global and / or
@@ -80,14 +75,12 @@ class ImageQualityCallback(Callback):
       If the return value is an nd.array, the results will be saved
       as separate scalar values in the tensorboard results
       (one for each value in the array)
-
     filter : dict of filters
       Filter to be applied before calculating all metrics and statistics.
       The key of the dict is the name of the filter which will be used to identify
       the results in the tensorboard event files.
       If you don't want any of the post-smoothed metrics, pass an empty dict.
       Default {}
-
     voxel_size_mm : tuple of floats
       Gives the voxel size in mm (z, y, x).
     """
@@ -139,42 +132,39 @@ class ImageQualityCallback(Callback):
         # (0) objective value
         self.tb_summary_writer.add_scalar('objective', objective, iteration)
 
-        test_image_array = test_image.as_array()
-        reference_image_array = self.reference_image.as_array()
+        test_im_arr, ref_im_arr = test_image.as_array(), self.reference_image.as_array()
 
         # for post_smoothing_fwhm_mm in self.post_smoothing_fwhms_mm_list:
         for filter_name, filter_func in self.filter.items():
-            if filter_func is None:
-                test_image_array_ps = test_image_array
-                reference_image_array_ps = reference_image_array
-            else:
-                test_image_array_ps = filter_func(test_image_array)
-                reference_image_array_ps = filter_func(reference_image_array)
-
-            def log_metrics_stats(ref_im, test_im, prefix="Global_"):
-                for metric_name, metric in self.metrics.items():
-                    met = metric(ref_im, test_im)
-                    # check if metric is scalar or vector valued
-                    # for the 2nd case, we save each scalar value separately in the dict
-                    if isinstance(met, np.ndarray):
-                        for im, m in enumerate(met.ravel()):
-                            self.tb_summary_writer.add_scalar(f'{prefix}{metric_name}_{im}{filter_name}', m, iteration)
-                    else:
-                        self.tb_summary_writer.add_scalar(f'{prefix}{metric_name}{filter_name}', met, iteration)
-
-                for statistic_name, statistic in self.statistics.items():
-                    stat = statistic(test_im)
-                    # check if statistic is scalar or vector valued
-                    # for the 2nd case, we save each scalar value separately in the dict
-                    if isinstance(stat, np.ndarray):
-                        for ist, st in enumerate(stat.ravel()):
-                            self.tb_summary_writer.add_scalar(f'{prefix}{statistic_name}_{ist}{filter_name}', st, iteration)
-                    else:
-                        self.tb_summary_writer.add_scalar(f'{prefix}{statistic_name}{filter_name}', stat, iteration)
+            if filter_func is not None:
+                test_im, ref_im = map(filter_func, (test_im_arr, ref_im_arr))
 
             # (1) global metrics & statistics
-            log_metrics_stats(reference_image_array_ps.ravel(), test_image_array_ps.ravel(), "Global_")
+            self._log_metrics_stats(ref_im.ravel(), test_im.ravel(), "Global_",
+                                    filter_name, iteration)
 
             # (2) local metrics & statistics
             for roi_name, roi_inds in self.roi_indices.items():
-                log_metrics_stats(reference_image_array_ps[roi_inds], test_image_array_ps[roi_inds], f"Local_{roi_name}_")
+                self._log_metrics_stats(ref_im[roi_inds], test_im[roi_inds],
+                                        f"Local_{roi_name}_", filter_name, iteration)
+
+    def _log_metrics_stats(self, ref_im, test_im, prefix, suffix, iteration):
+        for metric_name, metric in self.metrics.items():
+            met = metric(ref_im, test_im)
+            # check if metric is scalar or vector valued
+            # for the 2nd case, we save each scalar value separately in the dict
+            if isinstance(met, np.ndarray):
+                for im, m in enumerate(met.ravel()):
+                    self.tb_summary_writer.add_scalar(f'{prefix}{metric_name}_{im}{suffix}', m, iteration)
+            else:
+                self.tb_summary_writer.add_scalar(f'{prefix}{metric_name}{suffix}', met, iteration)
+
+        for statistic_name, statistic in self.statistics.items():
+            stat = statistic(test_im)
+            # check if statistic is scalar or vector valued
+            # for the 2nd case, we save each scalar value separately in the dict
+            if isinstance(stat, np.ndarray):
+                for ist, st in enumerate(stat.ravel()):
+                    self.tb_summary_writer.add_scalar(f'{prefix}{statistic_name}_{ist}{suffix}', st, iteration)
+            else:
+                self.tb_summary_writer.add_scalar(f'{prefix}{statistic_name}{suffix}', stat, iteration)
